a087f62e58d0d540d7f441b9fbe1ddef8e4b4266
Rutwik Balkhande <rutwikbalkhande42@gmail.com>
2025-06-06 23:43:41 +0530

    Features URL Added in Swagger (#3)
    twom url created in 1st is production 2nd local dev

--- src/main/java/com/example/EmployeeManagementSystem/configuration/SwaggerConfig.java
+++ src/main/java/com/example/EmployeeManagementSystem/configuration/SwaggerConfig.java
@@ -2,22 +2,26 @@ package com.example.EmployeeManagementSystem.configuration;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
 
-import java.awt.event.WindowFocusListener;
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
 
-    @Bean                                        //Swagger URl = http://localhost:8080/swagger-ui.html
-    public OpenAPI customOpenAPI()
-    {
+    @Bean // Swagger URL = http://localhost:8080/swagger-ui.html
+    public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .info(new Info()
                         .title("Employee Project Management API")
                         .version("1.0")
-                        .description("\"API documentation for Employee Management System") );
+                        .description("API documentation for Employee Management System")
+                )
+                .servers(List.of(
+                        new Server().url("https://api.provatosoft.com/v1").description("Production"),
+                        new Server().url("http://localhost:8080").description("Local Dev")
+                ));
     }
 }
